447739fd7de82d4368a76c6006dd1d174230180d
minaylosd <minaylosd@gmail.com>
2023-10-25 03:45:37 +0400

    fix apos

--- src/components/Counterparty/Counterparty.jsx
+++ src/components/Counterparty/Counterparty.jsx
@@ -12,12 +12,12 @@ export const Counterparty = () => {
           <div className={styles.card}>
             <p className={styles.subHeading}>Deals from 20 000$</p>
             <p className={styles.description}>
-              If you’re already working with UAE and facing delays, quality
+              If you&apos;re already working with UAE and facing delays, quality
               issues, or untrustworthy supplier issues, verify information
               accuracy, optimize logistics or, in some cases, reach directly to
               the manufacturer, bypassing intermediate suppliers. If you need a
               partner to handle contract changes, payment schedules, and resolve
-              supplier problems, we're here to assist and ensure reliable
+              supplier problems, we&apos;re here to assist and ensure reliable
               supplies, saving you from any unnecessary headaches.
             </p>
           </div>
