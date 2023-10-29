6ccc8d4b8ad1f1b6fdcba65eb977a1ba1aa5bf0d
minaylosd <minaylosd@gmail.com>
2023-10-29 13:03:09 +0400

    fix config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  assetPrefix: "./",
-  reactStrictMode: true,
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
